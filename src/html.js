16aa6f44c21846542fe237fecf4894492bb49708
Kimminsu <alstn2468_@naver.com>
2020-02-03 17:48:22 +0900

    Add canonical tag

--- src/html.js
+++ src/html.js
@@ -16,6 +16,7 @@ export default class HTML extends React.Component {
             name="google-site-verification"
             content="rur6f0C6BHHOpbBSHH53vuHLN81zkTeSO6F3nIBNleY"
           />
+          <link rel="canonical" href="https://alstn2468.github.io/" />
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
