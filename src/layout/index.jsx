1f985386332a58dfce1230549c58e76650c74d45
Kimminsu <alstn2468_@naver.com>
2020-05-11 16:59:28 +0900

    Fix layout's maxWidth rhythm parameter

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -18,7 +18,7 @@ export const Layout = ({ location, title, children }) => {
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(28),
+          maxWidth: rhythm(31),
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
