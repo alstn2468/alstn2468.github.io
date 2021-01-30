0c325b87b88c730fbdf28166a7f9ee0000129ef9
alstn2468 <alstn2468_@naver.com>
2021-01-30 20:30:25 +0900

    html.js에 있는 canonical관련 태그 삭제

--- src/html.js
+++ src/html.js
@@ -20,7 +20,6 @@ export default class HTML extends React.Component {
             name="naver-site-verification"
             content="827b1a994f25a73917bc2cf204f2021eacea978a"
           />
-          <link rel="canonical" href="https://alstn2468.github.io/" />
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
