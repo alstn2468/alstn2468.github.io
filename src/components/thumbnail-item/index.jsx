04534630abd47d0ed15d68c1ab57ce5bb9872893
Minsu Kim <alstn2468_@naver.com>
2021-04-01 13:26:22 +0900

    스타일 추가

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -8,6 +8,8 @@ export const ThumbnailItem = ({ node }) => (
   <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
     <div key={node.fields.slug}>
       <h3>{node.frontmatter.title || node.fields.slug}</h3>
+      <h4>{node.frontmatter.category}</h4>
+      <small>{node.frontmatter.date}</small>
       <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
     </div>
   </Link>
